13bba8a044d97274ecfcbca1379e504c569c4acb
joshJarr <josh.jarvis@potatolondon.com>
2020-12-05 15:29:11 +0000

    Custom inkywhat scriipt

--- days-till-christmas.py
+++ days-till-christmas.py
@@ -1,43 +1,21 @@
-#!/usr/bin/env python
+from inky import InkyWHAT
+from PIL import Image, ImageFont, ImageDraw
+from font_fredoka_one import FredokaOne
 
-import sys
+inky_display = InkyWHAT("red")
+inky_display.set_border(inky_display.WHITE)
 
-from PIL import ImageFont
 
-import inkyphat
+img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
+draw = ImageDraw.Draw(img)
 
-#inkyphat.set_rotation(180)
+font = ImageFont.truetype(FredokaOne, 36)
 
-colour = 'red'
-inkyphat.set_colour(colour)
+message = "20 Days Unil Christmas!"
+w, h = font.getsize(message)
+x = (inky_display.WIDTH / 2) - (w / 2)
+y = (inky_display.HEIGHT / 2) - (h / 2)
 
-# Show the backdrop image
-
-inkyphat.set_border(inkyphat.RED)
-
-# Partial update if using Inky pHAT display v1
-
-if inkyphat.get_version() == 1:
-    inkyphat.show()
-
-# Add the text
-
-font = ImageFont.truetype(inkyphat.fonts.AmaticSCBold, 38)
-
-text = '20 Days until Christmas'
-
-w, h = font.getsize(text)
-
-# Center the text and align it with the name strip
-
-x = (inkyphat.WIDTH / 2) - (w / 2)
-y = 71 - (h / 2)
-
-inkyphat.text((x, y), text, inkyphat.BLACK, font)
-
-# Partial update if using Inky pHAT display v1
-
-if inkyphat.get_version() == 1:
-    inkyphat.set_partial_mode(56, 96, 0, inkyphat.WIDTH)
-
-inkyphat.show()
+draw.text((x, y), message, inky_display.RED, font)
+inky_display.set_image(img)
+inky_display.show()
