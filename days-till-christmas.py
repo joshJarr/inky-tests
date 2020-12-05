b7d1f734ba7896a925d8ec8c17e9d1dd740f86fe
joshJarr <josh.jarvis@potatolondon.com>
2020-12-05 15:43:07 +0000

    Addeed count to newline

--- days-till-christmas.py
+++ days-till-christmas.py
@@ -11,11 +11,33 @@ draw = ImageDraw.Draw(img)
 
 font = ImageFont.truetype(FredokaOne, 36)
 
-message = "20 Days Unil Christmas!"
-w, h = font.getsize(message)
-x = (inky_display.WIDTH / 2) - (w / 2)
-y = (inky_display.HEIGHT / 2) - (h / 2)
+show_countdown("20")
+show_message("Days Unil Christmas!")
 
-draw.text((x, y), message, inky_display.RED, font)
+
 inky_display.set_image(img)
 inky_display.show()
+
+def show_countdown(count):
+  w, h = font.getsize(count)
+  x = (inky_display.WIDTH / 2) - (w / 2)
+  y = (inky_display.HEIGHT) - (h / 2)
+
+  draw.text((
+    (inky_display.WIDTH / 2) - (w / 2),
+    (0),
+    count,
+    inky_display.RED, font
+  )
+
+def show_message(message):
+  w, h = font.getsize(message)
+  x = (inky_display.WIDTH / 2) - (w / 2)
+  y = (inky_display.HEIGHT) - (h / 2)
+
+  draw.text((
+    (inky_display.WIDTH / 2) - (w / 2),
+    (inky_display.HEIGHT) - (h / 2)),
+    message,
+    inky_display.RED, font
+  )
